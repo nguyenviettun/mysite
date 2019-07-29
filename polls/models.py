cf10e8a4b4ea7a4a213a54dc1b1c9ee930de345f
nguyenviettun <tuan.nguyen@employmenthero.com>
2019-07-29 21:19:11 +0700

    Import timezone

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
+from django.utils import timezone
 from django.db import models
+import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -6,6 +8,9 @@ class Question(models.Model):
 
     def  __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -15,4 +20,3 @@ class Choice(models.Model):
 
     def  __str__(self):
         return self.choice_text
-
